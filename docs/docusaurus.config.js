d1cdb19d4bfac1b83b12555fb11a50fd69a95b69
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-02-19 11:01:04 -0300

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ponderada1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -45,7 +45,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-            routeBasePath: '/ponderada1/'
+            routeBasePath: '/'
         },
         blog: false,
         theme: {
