b8ef1dfd8cdfa27b6586906fa84a5541bb2fa2ab
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-02-20 15:14:03 -0300

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'a',
           },
           {
             href: 'https://github.com/facebook/docusaurus',
